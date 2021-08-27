39c6fb2469e9bcc45c087bbf3aa71561193a06bc
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-27 20:26:15 +0200

    integration et css de Presentation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header'
+import Presentation from './components/Presentation'
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Presentation />
 
     {/* 
     TODO:section 1 presentation
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,9 +4,12 @@ import presentationText from "./Presentation.json"
 function Presentation() {
   return (
     <div className="Presentation">
+    <h2>{presentationText[0].Paragraphe}</h2>
+    <h3>Un peu plus </h3>
+    <p>{presentationText[1].Paragraphe}</p>
 
     </div>
   );
 }
 
-export default Header;
+export default Presentation;
