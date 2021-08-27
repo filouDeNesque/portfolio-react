f1eb929f53c3ffa0faceab8c9e3ec68f0804fd65
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-27 12:26:46 +0200

    ajout du menu contact et du nom du site

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import './Header.css';
+import github from '../assets/logo/github.png';
+import mail from '../assets/logo/mail.png';
+import linkedin from '../assets/logo/linkedin.png';
 
-function App() {
+function Header() {
   return (
     <div className="Header">
-    <h2>Paul</h2>
-    {/* TODO:Menu flottant
-      TODO: contact bar (github, linkedin, mail)*/}
+    <h2>PAUL</h2>
+
+    <ul className="contact-float-right">
+
+    <li style ={{backgroundImage:`url(${mail})`}} ></li>
+    <li style ={{backgroundImage:`url(${linkedin})`}} ></li>
+    <li style ={{backgroundImage:`url(${github})`}} ></li>
+
+    </ul>
     </div>
   );
 }
