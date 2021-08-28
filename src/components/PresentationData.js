91a45595c7f414d444cf7100f6ef08fef5cefc7a
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-28 12:00:22 +0200

    ajout des dataPresentation

---
+++ src/components/PresentationData.js
@@ -0,0 +1,22 @@
+import './PresentationData.css';
+
+function PresentationData() {
+  return (
+    <div className="PresentationData">
+    <div className="dataColumn">
+    <h2>02</h2>
+    <p>Année de reconversion</p>
+    </div>
+    <div className="dataColumn">
+    <h2>100</h2>
+    <p><i class="fas fa-heart"></i>  du code </p>
+    </div>
+    <div className="dataColumn">
+    <h2><i class="fas fa-infinity"></i></h2>
+    <p>curieux</p>
+    </div>
+    </div>
+  );
+}
+
+export default PresentationData;
