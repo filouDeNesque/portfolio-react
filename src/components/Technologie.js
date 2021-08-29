2f66707970b433179132ed7a0fe6b9f9e6c33782
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-29 23:54:02 +0200

    ajout du mapping des data

--- src/components/Technologie.js
+++ src/components/Technologie.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import TechnologieData from './TechnologieData.json';
+import TechnologieText from './TechnologieText.json';
+
+
 
 export class Technologie extends  React.Component {
 
+
+
+
   render(){
-    return <h1>Component Technologie </h1>;
+    return <>
+      <h1>Component Technologie </h1>
+      <p>{TechnologieText[0].chapnumber}</p>
+      <p>{TechnologieData.map(data=>(
+	<p>{data.categorie}</p>))}</p>
+
+      </>;
   }
 
 }
