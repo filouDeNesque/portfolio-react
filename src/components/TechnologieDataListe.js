1883716f0fcbe3ade7d9a00049a41215b9d32efe
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-30 18:01:28 +0200

    amelioration du rendue de l'animation data liste

--- src/components/TechnologieDataListe.js
+++ src/components/TechnologieDataListe.js
@@ -4,6 +4,7 @@ import  "./TechnologieDataListe.css";
 
 export class TechnologieDataListe extends React.Component{
   myref = React.createRef();
+  container = React.createRef();
 
   constructor(props){
     super(props);
@@ -11,46 +12,47 @@ export class TechnologieDataListe extends React.Component{
     this.handleClick = this.handleClick.bind(this);
   }
 
-
   componentDidMount(){
   }
 
   componentDidUpdate(){
+    var arrayhtmlellement = []
+    arrayhtmlellement = this.container.current.children
+
     var basicTimeline = anime.timeline();
     basicTimeline
       .add({
-	targets:".show" ,
-	translateX: 20,
-	rotate: '1turn',
+	targets:arrayhtmlellement,
+	translateX: [20,0],
 	backgroundColor: '#FFF',
 	duration: 800,
-	delay:100
-      })
-      .add({
-	targets: ".items-techno",
-	translateX: 0,
-	backgroundColor: '#FFF',
-	duration: 800
-      })
-    ;
+	loop:true,
+	scale: [
+	  {value: .1, easing: 'easeOutSine', duration: 500},
+	  {value: 1, easing: 'easeInOutQuad', duration: 1200}
+	],
+	delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
+      });
   }
 
   handleClick(){
-    console.log("handle click isopen value : "+this.state.isopen)
+    console.log(this.container.current.children)
     this.setState({ isopen : !this.state.isopen })
   }
 
   render(){ 
     return(
-      <div className="TechnologieDataListe">
+      <div className="TechnologieDataListe" >
       <button
       className="btn-techno-liste"
       onClick={this.handleClick}>
       {this.props.categorie}
       </button>
+      <div ref={this.container}>
       { this.props.items.map(data=>(
 	<p 
 	className={this.state.isopen ? "items-techno show": "items-techno"} 
+	id={data.name}
 	ref={this.myref}
 	>  
 	<span className="iconeTechno" 
@@ -59,6 +61,7 @@ export class TechnologieDataListe extends React.Component{
 	</p>
       ))
       }
+      </div>
       </div>
     )
   }
