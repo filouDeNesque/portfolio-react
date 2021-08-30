78b9689f3fa76187dd14c2cc06982473501e1890
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-30 12:59:39 +0200

    mise en forme des data icone

--- src/components/TechnologieDataListe.js
+++ src/components/TechnologieDataListe.js
@@ -1,20 +1,28 @@
-import  "./TechnologieDataListe.css"
+import {useState} from "react";
+import  "./TechnologieDataListe.css";
 
 export default function  TechnologieDataListe(props){
+
+  const [isopen, setIsOpen] = useState(false);
 
   return(
 
     <div className="TechnologieDataListe">
-    <button> {props.categorie}</button>
+    <button
+    className="btn-techno-liste"
+    onClick={()=>setIsOpen(!isopen)}>
+    {props.categorie}
+    </button>
+    {isopen &&
+      props.items.map(data=>(
+	<p className="items-techno">  
+	<span className="iconeTechno" 
+	style ={{backgroundImage:`url(${data.icone})`}} />
+	{data.name}
+	</p>
+      ))
 
-    {props.items.map(data=>(
-      <p className="items-techno">  
-      <span className="iconeTechno" 
-      style ={{backgroundImage:`url(${data.icone})`}} />
-      {data.name}
-      </p>
-    ))}
-
+    }
     </div>
   )
 }	
