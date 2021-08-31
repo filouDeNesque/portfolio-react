2e5cc4bf540f93562b206990ea03a062f52cdb3b
FilouDeNesque <p.cuquemy@yahoo.com>
2021-08-31 10:54:51 +0200

    ajout du lien + modif css EcoleInfos

--- src/components/EcoleInfos.js
+++ src/components/EcoleInfos.js
@@ -9,21 +9,21 @@ function EcoleInfos(){
     <p> L 'Afcepf (EQL) est un organisme français de formation professionnelle et un établissement d’enseignement supérieur délivrant des diplômes d’État inscrits au RNCP. Spécialisée dans le domaine informatique études et développement et la reconversion professionnelle, elle dispense trois formations de niveau Bac+4 à Bac+5. AFCEPF et EQL (Ecole de la Qualité Logicielle) forment ensemble un établissement de formation professionnelle</p>
     </div> 
     <div className="ecole-container-column-right">
-<h3>Histoire: </h3>
+    <h3>Histoire: </h3>
     <p>L'Afcepf (Association française de conseil et d'éducation professionnelle) a été créée en 1984 avec l'appui de l'École Polytechnique Féminine. À l’origine, l’appellation de l’organisme était l’Association de Formation Continue de l’École Polytechnique Féminine. </p>
     <h4>Titre délivré </h4>
-   <ul>
+    <ul>
     <li>Titre « Analyste informaticien » (Reconversion professionnelle)
-Titre inscrit au RNCP (homologué par l’État) de niveau II (Master I - Bac + 4)
-Filière Génie du traitement de l’information (1er cycle) </li>
+  Titre inscrit au RNCP (homologué par l’État) de niveau II (Master I - Bac + 4)
+  Filière Génie du traitement de l’information (1er cycle) </li>
     <li> Titre « Architecte Logiciel »
-Titre inscrit au RNCP (homologué par l’État) de niveau I (Master 2 - Bac + 5)
-Filière Génie du traitement de l’information (2e cycle)</li>
+  Titre inscrit au RNCP (homologué par l’État) de niveau I (Master 2 - Bac + 5)
+  Filière Génie du traitement de l’information (2e cycle)</li>
     </ul>
-    <p>En savoir plus</p>
-  </div>
+    <p>En savoir <a href="https://www.ecoleql.fr/">plus</a></p>
     </div>
-  );
+    </div>
+);
 }
 
 export default EcoleInfos;
